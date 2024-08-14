300150d85ca2c7d5eaaeab7bfaf3b458e24912d1
gokulkumar0209 <84857047+gokulkumar0209@users.noreply.github.com>
2024-08-14 21:31:25 +0530

    NavBar UI created

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,6 @@
 
+import Home from './@components/elements/Home'
+import NavBar from './@components/elements/NavBar'
 import { Button } from './@components/ui/button'
 
 
@@ -7,8 +9,10 @@ function App() {
 
   return (
     <>
-     
-      <Button className=''>App</Button>
+    <div className=' bg-yellow-200 h-screen'>
+    <div className="fixed w-[68px] h-full left-0 z-1 top-0 bg-green-500 rounded-xl  "><NavBar/></div>
+    <div className=' h-screen w-full flex justify-center items-center'><Home/></div>
+    </div>
     </>
   )
 }
